f94a9916993a8e8f2190afe042cf14e84b37e6e8
DaPorkchop_ <daporkchop@daporkchop.net>
2025-02-17 16:18:35 +0100

    tilerenderer: automatically adjust the initial capacity for the tile_images vector
    this reduces how often the vector's storage needs to be grown during rendering, which is a pretty expensive process

--- src/mapcraftercore/renderer/tilerenderer.cpp
+++ src/mapcraftercore/renderer/tilerenderer.cpp
@@ -127,13 +127,22 @@ namespace {
 }
 
 void TileRenderer::renderTile(const TilePos& tile_pos, RGBAImage& tile) {
+	static std::atomic<size_t> tile_images_maxsize(64 << 10); //64Ki
+
 	tile.setSize(getTileWidth(), getTileHeight());
 
+	size_t tile_images_capacity_value = tile_images_maxsize;
+
 	boost::container::vector<TileImage> tile_images;
-    tile_images.reserve(80000);
+    tile_images.reserve(tile_images_capacity_value * 2);
 	renderTopBlocks(tile_pos, tile_images);
 
-    size_t count = tile_images.size();
+	size_t count = tile_images.size();
+	if (count > tile_images_capacity_value
+	    && tile_images_maxsize.compare_exchange_strong(tile_images_capacity_value, count)) {
+		LOG(DEBUG) << "raised initial tile_images capacity to " << count;
+	}
+
     std::vector<TileImage*> tile_image_pointers(count);
     for (size_t i = 0; i < count; i++) {
         tile_image_pointers[i] = &tile_images[i];
